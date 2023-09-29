043ae4103ef639e00cc4436e6b0693415d7ac8eb
Wanderlei Santos <wanderleisantosdias@gmail.com>
2023-09-28 23:46:19 -0300

    refactor: Removed username field

--- api/MyTraining/MyTraining.Core/Entities/User.cs
+++ api/MyTraining/MyTraining.Core/Entities/User.cs
@@ -2,22 +2,20 @@ namespace MyTraining.Core.Entities;
 
 public class User : BaseEntity
 {
-    public User(string name, string lastName, string email, string userName, string password)
+    public User(string firstName, string lastName, string email, string password)
     {
-        Name = name;
+        FirstName = firstName;
         LastName = lastName;
         Email = email;
-        UserName = userName;
         Password = password;
         CreatedAt = DateTime.Now;
         UpdatedAt = DateTime.Now;
         Active = true;
     }
 
-    public string Name { get; private set; }
+    public string FirstName { get; private set; }
     public string LastName { get; private set; }
     public string Email { get; private set; }
-    public string UserName { get; private set; }
     public string Password { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
