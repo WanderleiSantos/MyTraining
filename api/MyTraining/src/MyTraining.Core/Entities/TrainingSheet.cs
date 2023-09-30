569e2afb974d59a2a3912230f27d61c6da8daa9e
Wanderlei Santos <wanderleisantosdias@gmail.com>
2023-09-30 16:47:55 -0300

    feat: Created Entity TrainingSheet

---
+++ api/MyTraining/src/MyTraining.Core/Entities/TrainingSheet.cs
@@ -0,0 +1,33 @@
+namespace MyTraining.Core.Entities;
+
+public class TrainingSheet : BaseEntity
+{
+    public TrainingSheet(string name, string timeExchange)
+    {
+        Name = name;
+        TimeExchange = timeExchange;
+    }
+
+    public string Name { get; private set; }
+    public string TimeExchange { get; private set; }
+    public bool Active { get; private set; }
+
+    public void Update(string name, string timeExchange)
+    {
+        base.Update();
+        Name = name;
+        TimeExchange = timeExchange;
+    }
+
+    public void Activate()
+    {
+        base.Update();
+        Active = true;
+    }
+
+    public void Deactivate()
+    {
+        base.Update();
+        Active = false;
+    }
+}
